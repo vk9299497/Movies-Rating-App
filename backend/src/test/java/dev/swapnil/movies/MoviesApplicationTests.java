9876936c27262a1a778f471509739fd40c4cb1ee
swap421 <swapnilsingh421@gmail.com>
2023-07-05 00:15:29 +0530

    Movie-rating-app

---
+++ backend/src/test/java/dev/swapnil/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.swapnil.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
